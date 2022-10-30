4408461442328753eddac3898d3e8fa0d9dbd43f
KushBlazingJudah <59340248+KushBlazingJudah@users.noreply.github.com>
2022-10-30 12:08:38 -0300

    move html stuff out of server.go; proxy headers

---
+++ html.go
@@ -0,0 +1,160 @@
+package boorumux
+
+import (
+	"context"
+	"fmt"
+	"html/template"
+	"net/http"
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/KushBlazingJudah/boorumux/booru"
+)
+
+var templates *template.Template
+var mapPool = sync.Pool{
+	New: func() any {
+		return map[string]interface{}{}
+	},
+}
+var ssPool = sync.Pool{
+	New: func() any {
+		return new([]string)
+	},
+}
+
+func init() {
+	// Compile all of the templates.
+	templates = template.Must(template.New("").Funcs(template.FuncMap{
+		"embed":     func() error { panic("embed called too early") },
+		"booruId":   func() error { panic("booruId called too early") },
+		"humantag":  func(s string) string { return strings.ReplaceAll(s, "_", " ") },
+		"size":      humanSize,
+		"pages":     buildPageBlock,
+		"isUrl":     schemaRegexp.MatchString,
+		"prettyUrl": prettyUrl,
+		"concat":    func(s []string, c string) string { return strings.Join(s, c) },
+		"ver":       func() string { return "0.0.0" }, // TODO
+	}).ParseGlob("./views/*.html"))
+}
+
+func checkin(d map[string]interface{}) {
+	for k := range d {
+		delete(d, k)
+	}
+	mapPool.Put(d)
+}
+
+func (s *Server) pageHandler(w http.ResponseWriter, r *http.Request, targetBooru string, page int, tags []string) {
+	data, _, err := s.Boorus[targetBooru].Page(context.TODO(), booru.Query{Tags: tags}, page)
+	if err != nil {
+		panic(err)
+	}
+
+	// Filter out blacklisted tags
+	// This looks weird but trust me on this; it's simply an in-place filter.
+	n := 0
+	for _, v := range data {
+		fine := true
+		for _, f := range s.Blacklist {
+			if f.Match(&v) {
+				fine = false
+				break
+			}
+		}
+
+		if fine {
+			data[n] = v
+			n++
+		}
+	}
+	data = data[:n]
+
+	// Find all of the tags on this page
+	ssptr := ssPool.Get().(*[]string)
+	defer ssPool.Put(ssptr)
+
+	ss := *ssptr
+	ss = ss[:0]
+
+	for _, p := range data {
+		for _, v := range p.Tags {
+			ok := true
+
+			for _, k := range tags {
+				if v == k {
+					ok = false
+					break
+				}
+			}
+
+			if ok {
+				ss = append(ss, v)
+			}
+		}
+	}
+
+	pageTags := mostCommon(ss)
+	if len(pageTags) > maxSidebarTags {
+		pageTags = pageTags[:maxSidebarTags]
+	}
+
+	// Render it out
+	tmpldata := mapPool.Get().(map[string]interface{})
+	defer checkin(tmpldata)
+
+	tmpldata["booru"] = targetBooru
+	tmpldata["boorus"] = s.boorus
+	tmpldata["activeTags"] = tags
+	tmpldata["tags"] = pageTags
+	tmpldata["posts"] = data
+	tmpldata["page"] = page
+	tmpldata["q"] = r.URL.Query().Get("q")
+
+	if tmpldata["q"] == "" {
+		tmpldata["title"] = fmt.Sprintf("%s #%d - Boorumux", targetBooru, page)
+	} else {
+		tmpldata["title"] = fmt.Sprintf("%s #%d - Boorumux", tmpldata["q"], page)
+	}
+
+	templates.Funcs(template.FuncMap{
+		"embed": func() error {
+			return templates.Lookup("page.html").Execute(w, tmpldata)
+		},
+		"booruId": func(b booru.API) string {
+			for k, v := range s.Boorus {
+				if v == b {
+					return k
+				}
+			}
+			return ""
+		},
+	}).ExecuteTemplate(w, "main.html", tmpldata)
+}
+
+func (s *Server) postHandler(w http.ResponseWriter, r *http.Request, targetBooru string, id int) {
+	data, err := s.Boorus[targetBooru].Post(context.TODO(), id)
+	if err != nil {
+		panic(err)
+	}
+
+	// Sort it out
+	sort.Strings(data.Tags)
+
+	// Render it out
+	tmpldata := mapPool.Get().(map[string]interface{})
+	defer checkin(tmpldata)
+
+	tmpldata["title"] = fmt.Sprintf("%s on %s - Boorumux", strings.Join(data.Tags, " "), targetBooru)
+	tmpldata["booru"] = targetBooru
+	tmpldata["boorus"] = s.boorus
+	tmpldata["tags"] = data.Tags
+	tmpldata["post"] = data
+	tmpldata["q"] = r.URL.Query().Get("q")
+	tmpldata["from"] = r.URL.Query().Get("from")
+
+	templates.Funcs(template.FuncMap{"embed": func() error {
+		return templates.Lookup("post.html").Execute(w, tmpldata)
+	}}).ExecuteTemplate(w, "main.html", tmpldata)
+}
--- server.go
+++ server.go
@@ -1,14 +1,11 @@
 package boorumux
 
 import (
-	"context"
-	"fmt"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"regexp"
-	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -16,19 +13,6 @@ import (
 	"github.com/KushBlazingJudah/boorumux/booru"
 	"github.com/KushBlazingJudah/boorumux/filter"
 )
-
-var templates *template.Template
-var indexRegexp = regexp.MustCompile(`^/([0-9a-z+]+)/?$`)
-var mapPool = sync.Pool{
-	New: func() any {
-		return map[string]interface{}{}
-	},
-}
-var ssPool = sync.Pool{
-	New: func() any {
-		return new([]string)
-	},
-}
 
 type reqType uint
 
@@ -41,6 +25,26 @@ const (
 const (
 	maxSidebarTags = 25
 )
+
+var indexRegexp = regexp.MustCompile(`^/([0-9a-z+]+)/?$`)
+
+// proxyReqHeaders is a list of headers that are sent with a proxy request to a
+// booru.
+var proxyReqHeaders = []string{
+	"Range",
+}
+
+// proxyRespHeaders is a list of headers copied from the response from the
+// server for a proxy request.
+var proxyRespHeaders = []string{
+	"Content-Type",
+	"Content-Length",
+	"Accept-Ranges",
+	"Last-Modified",
+	"Etag",
+	"Expires",
+	"Cache-Control",
+}
 
 // Server holds the main configuration for Boorumux and doubles as a
 // http.Handler.
@@ -64,28 +68,6 @@ type Server struct {
 	boorus []string
 
 	sync.Mutex
-}
-
-func init() {
-	// Compile all of the templates.
-	templates = template.Must(template.New("").Funcs(template.FuncMap{
-		"embed":     func() error { panic("embed called too early") },
-		"booruId":   func() error { panic("booruId called too early") },
-		"humantag":  func(s string) string { return strings.ReplaceAll(s, "_", " ") },
-		"size":      humanSize,
-		"pages":     buildPageBlock,
-		"isUrl":     schemaRegexp.MatchString,
-		"prettyUrl": prettyUrl,
-		"concat":    func(s []string, c string) string { return strings.Join(s, c) },
-		"ver":       func() string { return "0.0.0" }, // TODO
-	}).ParseGlob("./views/*.html"))
-}
-
-func checkin(d map[string]interface{}) {
-	for k := range d {
-		delete(d, k)
-	}
-	mapPool.Put(d)
 }
 
 // ServeHTTP serves a requested page.
@@ -175,119 +157,6 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s *Server) pageHandler(w http.ResponseWriter, r *http.Request, targetBooru string, page int, tags []string) {
-	data, _, err := s.Boorus[targetBooru].Page(context.TODO(), booru.Query{Tags: tags}, page)
-	if err != nil {
-		panic(err)
-	}
-
-	// Filter out blacklisted tags
-	// This looks weird but trust me on this; it's simply an in-place filter.
-	n := 0
-	for _, v := range data {
-		fine := true
-		for _, f := range s.Blacklist {
-			if f.Match(&v) {
-				fine = false
-				break
-			}
-		}
-
-		if fine {
-			data[n] = v
-			n++
-		}
-	}
-	data = data[:n]
-
-	// Find all of the tags on this page
-	ssptr := ssPool.Get().(*[]string)
-	defer ssPool.Put(ssptr)
-
-	ss := *ssptr
-	ss = ss[:0]
-
-	for _, p := range data {
-		for _, v := range p.Tags {
-			ok := true
-
-			for _, k := range tags {
-				if v == k {
-					ok = false
-					break
-				}
-			}
-
-			if ok {
-				ss = append(ss, v)
-			}
-		}
-	}
-
-	pageTags := mostCommon(ss)
-	if len(pageTags) > maxSidebarTags {
-		pageTags = pageTags[:maxSidebarTags]
-	}
-
-	// Render it out
-	tmpldata := mapPool.Get().(map[string]interface{})
-	defer checkin(tmpldata)
-
-	tmpldata["booru"] = targetBooru
-	tmpldata["boorus"] = s.boorus
-	tmpldata["activeTags"] = tags
-	tmpldata["tags"] = pageTags
-	tmpldata["posts"] = data
-	tmpldata["page"] = page
-	tmpldata["q"] = r.URL.Query().Get("q")
-
-	if tmpldata["q"] == "" {
-		tmpldata["title"] = fmt.Sprintf("%s #%d - Boorumux", targetBooru, page)
-	} else {
-		tmpldata["title"] = fmt.Sprintf("%s #%d - Boorumux", tmpldata["q"], page)
-	}
-
-	templates.Funcs(template.FuncMap{
-		"embed": func() error {
-			return templates.Lookup("page.html").Execute(w, tmpldata)
-		},
-		"booruId": func(b booru.API) string {
-			for k, v := range s.Boorus {
-				if v == b {
-					return k
-				}
-			}
-			return ""
-		},
-	}).ExecuteTemplate(w, "main.html", tmpldata)
-}
-
-func (s *Server) postHandler(w http.ResponseWriter, r *http.Request, targetBooru string, id int) {
-	data, err := s.Boorus[targetBooru].Post(context.TODO(), id)
-	if err != nil {
-		panic(err)
-	}
-
-	// Sort it out
-	sort.Strings(data.Tags)
-
-	// Render it out
-	tmpldata := mapPool.Get().(map[string]interface{})
-	defer checkin(tmpldata)
-
-	tmpldata["title"] = fmt.Sprintf("%s on %s - Boorumux", strings.Join(data.Tags, " "), targetBooru)
-	tmpldata["booru"] = targetBooru
-	tmpldata["boorus"] = s.boorus
-	tmpldata["tags"] = data.Tags
-	tmpldata["post"] = data
-	tmpldata["q"] = r.URL.Query().Get("q")
-	tmpldata["from"] = r.URL.Query().Get("from")
-
-	templates.Funcs(template.FuncMap{"embed": func() error {
-		return templates.Lookup("post.html").Execute(w, tmpldata)
-	}}).ExecuteTemplate(w, "main.html", tmpldata)
-}
-
 func (s *Server) proxyHandler(w http.ResponseWriter, r *http.Request, targetBooru string, target string) {
 	defer func() {
 		// XXX: There's this really annoying bug and I'm not sure if it's our
@@ -309,14 +178,25 @@ func (s *Server) proxyHandler(w http.ResponseWriter, r *http.Request, targetBoor
 		panic(err)
 	}
 
+	// Copy over request headers if they're there
+	for _, k := range proxyReqHeaders {
+		if v := r.Header.Get(k); v != "" {
+			req.Header.Set(k, v)
+		}
+	}
+
 	res, err := s.Boorus[targetBooru].HTTP().Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
 
-	w.Header().Set("Content-Length", fmt.Sprint(res.ContentLength))
-	w.Header().Set("Content-Type", res.Header.Get("Content-Type"))
+	// Copy over some headers if they're there
+	for _, k := range proxyRespHeaders {
+		if v := res.Header.Get(k); v != "" {
+			w.Header().Set(k, v)
+		}
+	}
 
 	// This is where the aforementioned bug occurs.
 	if _, err := io.Copy(w, res.Body); err != nil {
