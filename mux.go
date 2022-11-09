6112bb55b55e50c69644860a9b96cfa77d8c728e
KushBlazingJudah <59340248+KushBlazingJudah@users.noreply.github.com>
2022-11-09 17:50:13 -0400

    mux: several reqs at once, sorting

--- mux.go
+++ mux.go
@@ -2,7 +2,11 @@ package boorumux
 
 import (
 	"context"
+	"errors"
 	"net/http"
+	"sort"
+	"sync"
+	"sync/atomic"
 
 	"github.com/KushBlazingJudah/boorumux/booru"
 )
@@ -15,18 +19,50 @@ type Mux struct {
 }
 
 func (m *Mux) Page(ctx context.Context, q booru.Query, page int) ([]booru.Post, int, error) {
-	// TODO: Send multiple requests at once.
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-	results := []booru.Post{}
+	wg := sync.WaitGroup{}
+	var n int32 = 0
+	var cerr error
+
+	dc := make(chan []booru.Post, len(m.Boorus))
 
 	for _, v := range m.Boorus {
-		r, _, err := v.Page(ctx, q, page)
-		if err != nil {
-			return results, -1, err
-		}
+		wg.Add(1)
 
-		results = append(results, r...)
+		go func(b booru.API) {
+			defer wg.Done()
+
+			r, _, err := b.Page(ctx, q, page)
+			if err != nil && !errors.Is(err, context.Canceled) {
+				cerr = err
+				cancel()
+				return
+			}
+
+			if err == nil {
+				dc <- r
+				atomic.AddInt32(&n, int32(len(r)))
+			}
+		}(v)
 	}
+
+	wg.Wait()
+	close(dc)
+
+	if cerr != nil {
+		return nil, -1, cerr
+	}
+
+	results := make([]booru.Post, 0, n)
+	for v := range dc {
+		results = append(results, v...)
+	}
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].Created.After(results[j].Created)
+	})
 
 	return results, -1, nil
 }
